aaf10fbf3aeb8db2dc07e3ecb7f23522d31f7078
Dmitriy <ka1deo@mail.ru>
2023-11-01 21:36:17 +0300

    fix: added basename to routers

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,11 +4,11 @@ from rest_framework import routers
 from api.views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'groups', GroupViewSet)
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'groups', GroupViewSet, basename='groups')
 router.register(
     r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
-router.register(r'follow', FollowViewSet)
+router.register(r'follow', FollowViewSet, basename='follow')
 
 
 urlpatterns = [
